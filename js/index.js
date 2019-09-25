0a8a86057454f855559e128901c3786a0c6fc6a3
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-09-25 17:20:07 -0500

    Slides now accessible through footer navbar. Gave each slide object a header to be displayed on the footer and a paragraph element. Each paragraph element gives a desctiption of what each slide's content will consist of. (bug - each slide adds a p with a div to the main html element but doesn't delete previous slide's content)

--- js/index.js
+++ js/index.js
@@ -1,52 +1,121 @@
 (function () {
-    var main = document.getElementById("mainContent");
+    var currentSlide = 0;
+    // ^ Current slide displayed
+
+    var displayMain = document.getElementById("mainContent");
     // ^ Main Div
     
     var previous = document.getElementById("prevSlide");
     var next = document.getElementById("nextSlide");
-    var display = document.getElementById("displayNav");
+    var displayHead = document.getElementById("displayHeader");
     // ^ Mobile Nav-bar variables
-
-    var div = document.createElement("div");
-    var p = document.createElement("p");
-    // ^ Create elements
 
     var mySlides = [];
 
     var slide0 = {
         header: "About",
+        p: {
+            a: "This is my about.",
+        },
     }
     
     var slide1 = {
         header: "Education",
+        p: {
+            a: "List education and give brief summary of each.",
+        },
     }
 
     var slide2 = {
         header: "Skills",
+        p: {
+            a: "Go over the different languages I know and list the diffent skills acquired at BcSkills.",
+        },
     }
 
     var slide3= {
         header: "Interest",
+        p: {
+            a: "List my the different passions I have and things that I find of interest.",
+        }
     }
 
     var slide4 = {
         header: "Projects",
+        p: {
+            a: "List some projects I've finished and display a couple here.",
+        },
     }
 
     var slide5 = {
         header: "Contact",
+        p: {
+            a: "Give my Email, GitHub, LinkedIn links.",
+        }
     }
 
     mySlides.push(slide0, slide1, slide2, slide3, slide4, slide5);
 
-    console.log(mySlides);
+    function createSlides() {
 
 
+        var displaySlide = "";
+
+        switch (currentSlide) {
+            case 1:
+                displaySlide = slide1;
+                break;
+            case 2:
+                displaySlide = slide2;
+                break;
+            case 3:
+                displaySlide = slide3;
+                break;
+            case 4:
+                displaySlide = slide4;
+                break;
+            case 5:
+                displaySlide = slide5;
+                break;
+            default:
+                displaySlide = slide0;
+        }
+
+        displayHead.innerText = displaySlide.header;
+
+        var div = document.createElement("div");
+        var p = document.createElement("p");
+
+        p.innerText = displaySlide.p.a;
+
+        div.appendChild(p);
+
+        displayMain.appendChild(div);
+    }
+
+    createSlides();
+
+    function nextSlide() {
+        if (currentSlide === 5) {
+            currentSlide = 0;
+        } else {
+            currentSlide++;
+        }
+        createSlides();
+    }
+
+    next.addEventListener("click", nextSlide);
+
+    function previousSlide() {
+        if (currentSlide === 0) {
+            currentSlide = 5;
+        } else {
+            currentSlide--;
+        }
+        createSlides();
+    }
+
+    previous.addEventListener("click", previousSlide);
 
 })();
 
-function createSlides() {
-    console
-}
-
-console.log(mySlides)
