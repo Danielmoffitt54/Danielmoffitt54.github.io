9ba518a2d45f17d4f6fe8b0b570bee1db850845d
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-09-30 03:29:40 -0500

    Removed interest slide and ajusted files for new format. Removed express.jpg and mongo-db.jgp. Added draft content and js to display slides 0-3.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@
     var about = document.getElementById("aboutBtn");
     var education = document.getElementById("educationBtn");
     var skills = document.getElementById("skillsBtn");
-    var interest = document.getElementById("interestBtn");
     var projects = document.getElementById("projectsBtn");
     var contact = document.getElementById("contactBtn");
     // ^ Create variables linked to divs indside index.html.
@@ -20,68 +19,103 @@
             a: "Daniel",
             b: "Moffitt",
         },
-        p: {
-            a: "bio...",
+        subHeader: {
+            a: "Summary",
+            b: "Goals",
+            c: "Work Experience",
+            d: "Interest"
+        },
+        text: {
+            a: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, cupiditate blanditiis? Illo minima saepe quod, adipisci velit iusto aut quam est inventore necessitatibus sit temporibus deleniti atque iure ducimus libero cumque ex ut veritatis pariatur quidem! Nisi repellendus alias sit hic explicabo dolores voluptate recusandae nulla. Sapiente, ipsa accusantium culpa dicta mollitia maiores pariatur fugiat iste consequatur aliquam assumenda odit voluptatem, doloremque facere nam aliquid, autem error nobis harum! Deleniti quasi sunt temporibus consectetur quaerat!",
+            b: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque porro quisquam. Nam, aperiam? Distinctio incidunt magnam molestias aperiam neque non voluptate sit eligendi voluptatibus quidem, architecto, tempore officiis corrupti nulla reiciendis dolorem perspiciatis consequatur earum nobis? Dignissimos quaerat ipsam, porro tenetur sint nostrum illum atque doloribus eos labore eveniet!",
+            c: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti saepe recusandae harum! Dolore eos recusandae expedita id, nihil quaerat! Reprehenderit alias nisi incidunt animi, nesciunt beatae illo qui? Dolore fugiat voluptatem quos natus distinctio ipsum molestias. Nesciunt eos, architecto id iste omnis dolores iusto qui ipsa exercitationem et at eligendi.",
+            d: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti saepe recusandae harum! Dolore eos recusandae expedita id, nihil quaerat! Reprehenderit alias nisi incidunt animi, nesciunt beatae illo qui? Dolore fugiat voluptatem quos natus distinctio ipsum molestias. Nesciunt eos, architecto id iste omnis dolores iusto qui ipsa exercitationem et at eligendi."
         },
     }
     // ^ slide0 end.
     
     var slide1 = {
         title: "Education",
-        p: {
-            a: "List education and give brief summary of each.",
+        intro: "List education and give brief summary of each.",
+        subHeader: {
+            a: "Mount Juliet High School",
+            b: "BCskills",
+        },
+        attended: {
+            a: "2009 - 2013",
+            b: "August 2019 - PRESENT",
+        },
+        location: {
+            a: "Mount Juliet, TN",
+            b: "Glasgow, KY",
+        },
+        description: {
+            a: "GPA - ",
+            b: "",
         },
     }
     // ^ slide1 end.
 
     var slide2 = {
         title: "Skills",
-        header: {
+        subHeader: {
             a: "Programing Languages & Tools",
-            b:""
+            b: "Additional",
         },
         icons: {
             src: {
                 a: "img/html5-brands.svg",
                 b: "img/css3-alt-brands.svg",
                 c: "img/js-square-brands.svg",
-                d: "img/mongo-db.jpg",
-                e: "img/express.jpg",
-                f: "img/react-brands.svg",
-                g: "img/node-js-brands.svg",
-                h: "img/bootstrap-brands.svg",
+                d: "img/react-brands.svg",
+                e: "img/node-js-brands.svg",
+                f: "img/bootstrap-brands.svg",
             },
             alt: {
                 a: "Html5",
                 b: "Css3",
                 c: "JavaScript",
-                d: "MongoDB",
-                e: "Express",
-                f: "React",
-                g: "NodeJS",
-                h: "Bootstrap",
+                d: "React",
+                e: "NodeJS",
+                f: "Bootstrap",
             }
+        },
+        ul: {
+            a: "MongoDB",
+            b: "Express",
         },
     }
     // ^ slide2 end.
 
-    var slide3= {
-        title: "Interest",
-        p: {
-            a: "List my the different passions I have and things that I find of interest.",
-        }
-    }
-    // ^ slide3 end.
-
-    var slide4 = {
+    var slide3 = {
         title: "Projects",
-        p: {
-            a: "List some projects I've finished and display a couple here.",
+        intro: "List below provides ",
+        finished: {
+            subHeader: {
+                a: "lorem",
+            },
+            description: {
+                a: "lorem",
+            },
+            type: {
+                a: "lorem",
+            }
         },
+        inProgress: {
+            subHeader: {
+                a: "",
+            },
+            description: {
+                a: "",
+            },
+            type: {
+                a: "",
+            }
+        }
     }
     // ^ slide4 end.
 
-    var slide5 = {
+    var slide4 = {
         title: "Contact",
         p: {
             a: "",
@@ -90,7 +124,7 @@
     // ^ slide5 end.
 
 
-    mySlides.push(slide0, slide1, slide2, slide3, slide4, slide5);
+    mySlides.push(slide0, slide1, slide2, slide3, slide4);
     // ^ Pushes all objects to be stored inside the array mySlides
 
     var currentSlide = 0;
@@ -111,14 +145,10 @@
                 break;
             case 3:
                 displaySlide = slide3;
-                displayInterest();
+                displayProjects();
                 break;
             case 4:
                 displaySlide = slide4;
-                displayProjects();
-                break;
-            case 5:
-                displaySlide = slide5;
                 displayContact();
                 break;
             default:
@@ -131,6 +161,9 @@
         // ^ Displays current slide to user
 
         function displayAbout() {
+            about.classList.add("activeSlide");
+            // ^ Gives button on monitor view higlighted background.
+
             var displayMain = document.getElementById("mainContent");
             // ^ Create variable displayMain linked to main element on index.html.
 
@@ -138,7 +171,41 @@
             divSlide.id = "slide";
             // ^ Create variable divSlide with id as "slide".
 
+            var firstName = document.createElement("h1");
+            firstName.classList.add("aboutName");
+            var lastName = document.createElement("h1");
+            lastName.classList.add("aboutName");
+            // ^ Create variable firstName and lastName with class aboutName.
 
+            firstName.innerText = displaySlide.header.a;
+            lastName.innerText = displaySlide.header.b;
+            // ^ Give firstName and lastName values of header a and b.
+
+            divSlide.appendChild(firstName);
+            divSlide.appendChild(lastName);
+            // ^ Send firstName and lastName to divSlide.
+
+            for (letter in displaySlide.subHeader) {
+                var contentBlock = document.createElement("div");
+                contentBlock.classList.add("contentBlock");
+                // ^ Create variable contentBlock with class contentBlock.
+
+                var subHeader = document.createElement("h3");
+                var text = document.createElement("p");
+                // ^ Create variables subHeader and text.
+    
+                subHeader.innerText = displaySlide.subHeader[letter];
+                text.innerText = displaySlide.text[letter];
+                // ^ Give subHeader and text values for the corresponding letter in object.
+
+                contentBlock.appendChild(subHeader);
+                contentBlock.appendChild(text);
+                // ^ Send subHeader and text inside aboutCo
+
+                divSlide.appendChild(contentBlock);
+                // ^ Send aboutBlock inside divSlide.
+            }
+            // ^ For each letter inside the subHeader and text objects of slide0. Create a h3 and p element with their values.
 
             displayMain.appendChild(divSlide);
             // ^ Send divSlide inside displayMain to be displayed on index.html.
@@ -146,6 +213,9 @@
         // ^ displayAbout() end.
 
         function displayEducation() {
+            education.classList.add("activeSlide");
+            // ^ Gives button on monitor view higlighted background.
+
             var displayMain = document.getElementById("mainContent");
             // ^ Create variable displayMain linked to main element on index.html.
 
@@ -153,7 +223,32 @@
             divSlide.id = "slide";
             // ^ Create variable divSlide with id as "slide".
 
-            
+            for (letter in displaySlide.subHeader) {
+                var contentBlock = document.createElement("div");
+                contentBlock.classList.add("contentBlock");
+                // ^ Create variable contentBlock with class contentBlock.
+
+                var sub = document.createElement("h3");
+                var subInfo = document.createElement("h4");
+                var des = document.createElement("p");
+                // ^ Create variables sub, att, loc, and des.
+
+                var subInfoStr = displaySlide.attended[letter] + " | " + displaySlide.location[letter];
+                // ^ Creates variable for subInfoStr and gives value of corresponding letter of attended and loction object.
+
+                sub.innerText = displaySlide.subHeader[letter];
+                subInfo.innerText = subInfoStr;
+                des.innerText = displaySlide.description[letter];
+                // ^ Give value to variables sub, subInfo, and des, for their corresponding letter in objects.
+
+                contentBlock.appendChild(sub);
+                contentBlock.appendChild(subInfo);
+                contentBlock.appendChild(des);
+                // ^ Send sub, att, loc, and des, inside contentBlock.
+
+                divSlide.appendChild(contentBlock);
+                // ^ Send contentBlock inside divSlide.
+            }
 
             displayMain.appendChild(divSlide);
             // ^ Send divSlide inside displayMain to be displayed on index.html.
@@ -161,6 +256,9 @@
         // ^ displayEducation() end.
 
         function displaySkills() {
+            skills.classList.add("activeSlide");
+            // ^ Gives button on monitor view higlighted background.
+
             var displayMain = document.getElementById("mainContent");
             // ^ Create variable displayMain linked to main element on index.html.
 
@@ -168,47 +266,67 @@
             divSlide.id = "slide";
             // ^ Create variable divSlide with id as "slide".
 
-            var imgDiv= document.createElement("div");
-            // ^ Create variable imgDiv to store all img elements.
+            for (letter in displaySlide.subHeader) {
+                var subHeader = document.createElement("h3");
+                // ^ Create variable subHeader.
 
-            for (letter in displaySlide.icons.src) {
-                var img = document.createElement("img");
-                img.classList.add("skillsIcon");
-                // ^ Create variable img element with class skillsIcon.
+                subHeader.innerText = displaySlide.subHeader[letter];
+                // ^ Give subHeader value of displaySlide's subHeader value a.
 
-                img.src = displaySlide.icons.src[letter];
-                img.alt = displaySlide.icons.alt[letter];
-                // ^ Give img elements src and alt attributes.
+                divSlide.appendChild(subHeader);
+                // ^ Send subHeader to divSlide.
 
-                imgDiv.appendChild(img);
-                // ^ Send img inside imgDiv.
+                if (letter === "a") {
+                    var contentBlock = document.createElement("div");
+                    contentBlock.classList.add("contentBlock")
+                    // ^ Create variable contentBlock with class contentBlock.
+
+                    for (letter in displaySlide.icons.src) {
+                        var img = document.createElement("img");
+                        img.classList.add("skillsIcon");
+                        // ^ Create variable img element with class skillsIcon.
+
+                        img.src = displaySlide.icons.src[letter];
+                        img.alt = displaySlide.icons.alt[letter];
+                        // ^ Give img elements src and alt attributes for the corresponding letter in object.
+
+                        contentBlock.appendChild(img);
+                        // ^ Send img inside contentBlock.
+                    }
+                    // ^ For each letter inside the src and alt objects of slide2, create a img element with their attribute values and send inside contentBlock.
+                } else {
+                    var contentBlock = document.createElement("ul");
+                    contentBlock.classList.add("contentBlock");
+                    // ^ Create variable contentBlock with class contentBlock.
+
+                    for (letter in displaySlide.ul) {
+                        var listedItem = document.createElement("li");
+                        // ^ Create variable listedItem.
+
+                        listedItem.innerText = displaySlide.ul[letter];
+                        // ^ Give li elements the value for the corresponding letter in ul object.
+
+                        contentBlock.appendChild(listedItem);
+                        // ^ Send listedItem inside contentBlock.
+                    }
+                    // ^ For each letter inside the ul object of slide2, create a li element with their corresponding values and send inside contentBlock.
+                }
+                // ^ If subHeader value is a run the first, else run the second.
+
+                divSlide.appendChild(contentBlock);
+                // ^ Send contentBlock inside divSlide.
             }
-            // ^ For each letter inside the src and alt objects of slide2, create a img element with thier attribute values.
-
-            divSlide.appendChild(imgDiv);
-            // ^ Send imgDiv inside divSlide.
+            // ^ For each letter inside subHeader object of slide2, create a h3 element with value of subHeader's corresponding letter.
             
             displayMain.appendChild(divSlide);
             // ^ Send divSlide inside displayMain to be displayed on index.html.
         }
         // ^ displaySkills() end.
 
-        function displayInterest() {
-            var displayMain = document.getElementById("mainContent");
-            // ^ Create variable displayMain linked to main element on index.html.
-
-            var divSlide = document.createElement("div");
-            divSlide.id = "slide";
-            // ^ Create variable divSlide with id as "slide".
-
-            
-
-            displayMain.appendChild(divSlide);
-            // ^ Send divSlide inside displayMain to be displayed on index.html.
-        }
-        // ^ displayInterest() end.
-
         function displayProjects() {
+            projects.classList.add("activeSlide");
+            // ^ Gives button on monitor view higlighted background.
+
             var displayMain = document.getElementById("mainContent");
             // ^ Create variable displayMain linked to main element on index.html.
 
@@ -216,7 +334,45 @@
             divSlide.id = "slide";
             // ^ Create variable divSlide with id as "slide".
 
-            
+            for (letter in displaySlide.finished.subHeader) {
+                var projectBlock = document.createElement("div");
+                projectBlock.classList.add("projectBlock");
+                // ^ Create variable projectBlock with class projectBlock
+
+                var subHeader = document.createElement("h3");
+                var description = document.createElement("p");
+                var type = document.createElement("p");
+
+                subHeader.innerText = displaySlide.finished.subHeader[letter];
+                description.innerText = displaySlide.finished.description[letter];
+                type.innerText = displaySlide.finished.type[letter];
+
+                projectBlock.appendChild(subHeader);
+                projectBlock.appendChild(description);
+                projectBlock.appendChild(type);
+
+                divSlide.appendChild(projectBlock);
+            }
+
+            for (letter in displaySlide.inProgress.subHeader) {
+                var projectBlock = document.createElement("div");
+                projectBlock.classList.add("projectBlock");
+                // ^ Create variable projectBlock with class projectBlock
+
+                var subHeader = document.createElement("h3");
+                var description = document.createElement("p");
+                var type = document.createElement("p");
+
+                subHeader.innerText = displaySlide.inProgress.subHeader[letter];
+                description.innerText = displaySlide.inProgress.description[letter];
+                type.innerText = displaySlide.inProgress.type[letter];
+
+                projectBlock.appendChild(subHeader);
+                projectBlock.appendChild(description);
+                projectBlock.appendChild(type);
+
+                divSlide.appendChild(projectBlock);
+            } 
 
             displayMain.appendChild(divSlide);
             // ^ Send divSlide inside displayMain to be displayed on index.html.
@@ -224,6 +380,9 @@
         // ^ displayProjects() end.
 
         function displayContact() {
+            contact.classList.add("activeSlide");
+            // ^ Gives button on monitor view higlighted background.
+
             var displayMain = document.getElementById("mainContent");
             // ^ Create variable displayMain linked to main element on index.html.
 
@@ -246,13 +405,18 @@
     function deleteSlide() {
         if (document.getElementById("slide")) {
             document.getElementById("slide").remove();
+            about.classList.remove("activeSlide");
+            education.classList.remove("activeSlide");
+            skills.classList.remove("activeSlide");
+            projects.classList.remove("activeSlide");
+            contact.classList.remove("activeSlide");
         }
-        // ^ If the div with an id of "slide," is reachable delete div.
+        // ^ If the div with an id of "slide," is reachable delete div and remove class activeSlide from button.
     }
 
     function nextSlide() {
         deleteSlide();
-        if (currentSlide === 5) {
+        if (currentSlide === 4) {
             currentSlide = 0;
         } else {
             currentSlide++;
@@ -267,7 +431,7 @@
     function previousSlide() {
         deleteSlide();
         if (currentSlide === 0) {
-            currentSlide = 5;
+            currentSlide = 4;
         } else {
             currentSlide--;
         }
@@ -301,8 +465,7 @@
     about.addEventListener("click", function(){ selectNav(0); });
     education.addEventListener("click", function(){ selectNav(1); });
     skills.addEventListener("click", function(){ selectNav(2); });
-    interest.addEventListener("click", function(){ selectNav(3); });
-    projects.addEventListener("click", function(){ selectNav(4); });
-    contact.addEventListener("click", function(){ selectNav(5); });
+    projects.addEventListener("click", function(){ selectNav(3); });
+    contact.addEventListener("click", function(){ selectNav(4); });
     // ^ Listeners of function selectNav with values of the corresponding slides. 
 })();
